d6538a635ae3d09268a8e183aa8084503d7055fe
Akanksha174 <akankshakumari4755@gmail.com>
2025-01-14 21:00:29 +0530

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,32 @@
+cardData = [
+    {
+      suit: 'heart',
+      value: 7
+    },
+    {
+      suit: 'club',
+      value: 8
+    },
+    {
+      suit: 'club',
+      value: 2
+    },
+    {
+      suit: 'diamond',
+      value: 2
+    },
+    {
+      suit: 'diamond',
+      value: 5
+    },
+    {
+      suit: 'club',
+      value: 10
+    },
+  ]
+  
+  
+  function findCard(suit, value) {
+    
+  }
+  
